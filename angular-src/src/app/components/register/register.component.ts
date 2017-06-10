dc79442bb9312ec193425c28a1aaad2ef9e6f5f1
gaobowen1993 <gaobowen1993@gmail.com>
2017-06-10 11:15:33 -0700

    add angular folder

---
+++ angular-src/src/app/components/register/register.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(private validateSerivce: ValidateService,
+              private flashMessage: FlashMessagesService,
+              private authSerivce: AuthService,
+              private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    // Validate user required fields
+    if(!this.validateSerivce.validateRegister(user)) {
+      this.flashMessage.show('Please fill all the fields', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
+    // Validate email
+    if(!this.validateSerivce.validateEmail(user.email)) {
+      this.flashMessage.show('Please fill the validate email', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
+    // Register user
+    this.authSerivce.registerUser(user).subscribe(data => {
+      if(data.success) {
+        this.flashMessage.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
+        this.router.navigate(['/register']);
+      }
+    });
+  }
+}
